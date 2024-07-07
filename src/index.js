382f007480e9b10d51b3348e7626b8137eed270c
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2024-07-07 11:46:24 +0800

    Updated the Contents of the portfolio

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 // Prevent opening the console and disable left click
-document.addEventListener("contextmenu", function (e) {
-    e.preventDefault();
-}, false);
+// document.addEventListener("contextmenu", function (e) {
+//     e.preventDefault();
+// }, false);
 
-document.onkeydown = function (e) {
-    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C'))) {
-        e.preventDefault();
-    }
-};
+// document.onkeydown = function (e) {
+//     if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C'))) {
+//         e.preventDefault();
+//     }
+// };
 
 
 initScrollReveal(targetElements, defaultProps);
