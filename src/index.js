a4b16d659ec5372deb249b2be6b6c63c02b0010f
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2024-07-07 11:53:49 +0800

    chore: Prevent opening the console and disable left click

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
-// Prevent opening the console and disable left click
-// document.addEventListener("contextmenu", function (e) {
-//     e.preventDefault();
-// }, false);
+Prevent opening the console and disable left click
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+}, false);
 
-// document.onkeydown = function (e) {
-//     if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C'))) {
-//         e.preventDefault();
-//     }
-// };
+document.onkeydown = function (e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C'))) {
+        e.preventDefault();
+    }
+};
 
 
 initScrollReveal(targetElements, defaultProps);
